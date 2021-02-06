3a966f8551c722ff599e6128cb355d59715945e4
GoldElysium <48455312+GoldElysium@users.noreply.github.com>
2021-02-06 12:50:58 +0100

    Install grommet, remove eslint

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,32 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Head from 'next/head';
+import { useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	const [darkMode, setDarkMode] = useState(true);
+
+	return (
+		<>
+			<Head>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap"
+					rel="stylesheet"
+				/>
+				<title>Hololive Resistance Discord - Staff dashboard</title>
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+				<style>
+					{`
+                    * {
+                        font-family: 'Noto Sans', sans-serif;
+                    }
+                `}
+				</style>
+			</Head>
+			<Grommet fullthemeMode={darkMode ? 'dark' : 'light'}>
+				<Component {...pageProps} darkMode={darkMode} setDarkMode={setDarkMode} />
+			</Grommet>
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
