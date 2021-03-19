226a653903de91784b82c1039274b022ee09d772
GoldElysium <48455312+GoldElysium@users.noreply.github.com>
2021-03-19 13:07:12 +0100

    Package fixes

--- pages/translations.js
+++ pages/translations.js
@@ -1,7 +1,6 @@
 import Link from 'next/link';
-import Head from 'next/head';
-import React from 'react'
-import ReactPlayer from 'React-Player'
+import React from 'react';
+import ReactPlayer from 'react-player';
 import { Grommet, Header, Box, Button, Footer, Heading, Text} from 'grommet';
 
 export default function Translations() {
@@ -28,10 +27,10 @@ export default function Translations() {
                 <br/>
 
                 <Text>
-                    TL Team Description - "Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    TL Team Description - "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
                     Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
                 </Text>
                 <br/>
@@ -50,13 +49,13 @@ export default function Translations() {
                     {' '}
                     <Link href="/">
                         <a><strong>Home Page</strong></a>
-                    </Link>  
+                    </Link>
                 </main>
 
             </div>
             <Footer background={{color: 'black' }} direction="row" pad="small" justify="center">
                 <h2>This is a footer</h2>
-            </Footer>  
+            </Footer>
         </Grommet>
     )
   }
